e9625062bba52a786dbec1a084d26ea6651ab062
srosy <serosenvall@gmail.com>
2020-08-24 14:12:35 -0600

    A1_ArrayOperations done

---
+++ A01/ArrayOperations/ArrayOperations.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOperations
+{
+    public static class ArrayOperations
+    {
+        /// <summary>
+        /// Returns the max int from an int sequence in an array.
+        /// </summary>
+        /// <param name="intArray"></param>
+        /// <returns> int </returns>
+        public static int Max(int[] intArray)
+        {
+            return intArray.Max();
+        }
+
+        /// <summary>
+        /// Returns the min int from an int sequence in an array.
+        /// </summary>
+        /// <param name="intArray"></param>
+        /// <returns> int </returns>
+        public static int Min(int[] intArray)
+        {
+            return intArray.Min();
+        }
+
+        /// <summary>
+        /// Returns the average int from an int sequence in an array.
+        /// </summary>
+        /// <param name="intArray"></param>
+        /// <returns> int </returns>
+        public static double Avg(int[] intArray)
+        {
+            return intArray.Average();
+        }
+    }
+}
---
+++ A01/ArrayOperations/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Starting ArrayOperations ***");
+
+            Console.WriteLine("Please input the number of elements for the array: ");
+            int numArrayElements = int.Parse(Console.ReadLine());
+            int[] intArray = new int[numArrayElements];
+            Console.WriteLine();
+
+            Random random = new Random();
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                intArray[i] = random.Next(0, 100000);
+            }
+            Console.WriteLine($"Generated random int[{numArrayElements}]: [{string.Join(",", intArray)}]");
+            Console.WriteLine();
+
+            Console.WriteLine($"Max in array: {ArrayOperations.Max(intArray)}");
+            Console.WriteLine($"Min in array: {ArrayOperations.Min(intArray)}");
+            Console.WriteLine($"Average in array: {ArrayOperations.Avg(intArray)}");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
