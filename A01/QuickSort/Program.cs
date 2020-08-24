cb0c2a0e6aef47dea21eda91f9f28450c86ed8c9
srosy <serosenvall@gmail.com>
2020-08-24 15:07:26 -0600

    A1_QuickSort done

---
+++ A01/QuickSort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Starting Recursive QuickSort ***");
+            Console.WriteLine("Please enter the number of elements in the array: ");
+            int numElements = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            int[] unsortedArray = new int[numElements];
+            Random random = new Random();
+            for (int i = 0; i < unsortedArray.Length; i++)
+            {
+                unsortedArray[i] = random.Next(0, 100000);
+            }
+            Console.WriteLine($"Unsorted Array[{numElements}]: [{string.Join(",", unsortedArray)}]");
+            Console.WriteLine();
+
+            Console.WriteLine($"QuickSorted Array: {string.Join(",", QuickSort.SortItQuickRecursively(unsortedArray, 0, unsortedArray.Length - 1))}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ A01/QuickSort/QuickSort.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuickSort
+{
+    public static class QuickSort
+    {
+        public static int[] SortItQuickRecursively(int[] intArray, int left, int right)
+        {
+            if (left >= right)
+                return intArray;
+
+            int pivot = intArray[(left + right) / 2];
+            int index = Partition(intArray, left, right, pivot);
+            SortItQuickRecursively(intArray, left, index - 1);
+            SortItQuickRecursively(intArray, index, right);
+
+            return intArray;
+        }
+
+        public static int Partition(int[] intArray, int left, int right, int pivot)
+        {
+            while (left <= right)
+            {
+                while (intArray[left] < pivot) left++; // find next left element to swap
+                while (intArray[right] > pivot) right--; // find next right element to swap
+
+                if (left <= right)
+                {
+                    SwapElements(intArray, left, right);
+                    left++;
+                    right--;
+                }
+            }
+            return left; // partition point
+        }
+
+        private static void SwapElements(int[] intArray, int left, int right)
+        {
+            int temp = intArray[right];
+            intArray[right] = intArray[left];
+            intArray[left] = temp;
+        }
+    }
+}
